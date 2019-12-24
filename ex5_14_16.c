1d163a9365803bc5d4ccaa58a8a79b5c354c6920
YellowStar5 <codercuixin@gmail.com>
2019-12-24 10:23:16 +0800

    add part of chapter5

--- ex5_14_16.c
+++ ex5_14_16.c
@@ -1,14 +1,28 @@
 //
 // Created by cuixin on 2019-12-22.
 //gcc ex5_14
-//./a.out -r -n
-// todo 待更改
+//./a.out -f -d -r
+// DO YOU love ME
+//do you love me
+//DARGON
+//DO      you love me
+//DTO
+//^D
+//
+//
+//DTO
+//do you love me
+//DO YOU love ME
+//DO      you love me
+//DARGON
 //
 
 #include <stdio.h>
 #include <string.h>
 //通过排序的最大文本行数
 #define MAXLINES 5
+
+#include <ctype.h>
 
 char *lineptr[MAXLINES]; //指向文本行的指针数组
 
@@ -20,13 +34,9 @@ int getline_v1(char s[], int lim);
 
 void qsort_v1(void *lineptr[], int left, int right, int (*comp)(void *, void *));
 
-int numcmp(char *, char *);
+int cmp(char *s1, char *s2);
 
-int numcmp_reverse(char *s1, char *s2);
-
-int strcmp_reverse(char *s1, char *s2);
-
-int strcmpi_reverse(char *s1, char *s2);
+int strcmp_inner(char *s1, char *s2);
 
 char *alloc(int n);
 
@@ -51,41 +61,45 @@ void afree(char *p) { /*释放从p指向的存储区*/
     }
 }
 
+int numeric = 0; //若进行数值排序，则numeric的值为1
+int reverse = 0; //是否倒序排序。
+int caseSensitive = 1; //字符串比较时，大小写是否敏感
+int alphabetNumberSpaceOnly = 0;
+
 //对输入的文本行进行排序
 int main(int argc, char *argv[]) {
     int nlines; //读取的输入行数目
-    int numeric = 0; //若进行数值排序，则numeric的值为1
-    int reverse = 0; //是否倒序排序。
-    int caseSensitive = 1; //字符串比较时，大小写是否敏感
-    int alphabetNumberSpaceOnly = 0;
     while (argc-- > 1) {
-        if (strcmp(argv[argc], "-n") == 0) {
-            numeric = 1;
-        } else if (strcmp(argv[argc], "-r") == 0) {
-            reverse = 1;
-        } else if (strcmp(argv[argc], "-rn") == 0 || strcmp(argv[argc], "-nr") == 0) {
-            numeric = reverse = 1;
-        } else if (strcmp(argv[argc], "-f") == 0) {
-            caseSensitive = 0;
-        } else if (strcmp(argv[argc], "-d") == 0) {
-            alphabetNumberSpaceOnly = 1;
-        } else if (strcmp(argv[argc], "-df") == 0 || strcmp(argv[argc], "-fd") == 0) {
-            alphabetNumberSpaceOnly = 1;
-            caseSensitive = 0;
-        } else {
-            printf("error: don't match pattern sort -n -r");
+        char *p = *++argv;
+        if(*p++ == '-'){
+            while(*p != '\0'){
+                switch(*p){
+                    case 'n':
+                        numeric =1;
+                        break;
+                    case 'r':
+                        reverse = 1;
+                        break;
+                    case 'd':
+                        alphabetNumberSpaceOnly = 1;
+                        break;
+                    case 'f':
+                        caseSensitive = 0;
+                        break;
+                    default:
+                        printf("error arg -%s", p);
+                        return 1;
+                }
+                p++;
+            }
+        }else{
+            printf("error arg %s", p);
             return 1;
         }
     }
     if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-        if (!reverse) {
-            qsort_v1((void **) lineptr, 0, nlines - 1,
-                     (int (*)(void *, void *)) (numeric ? numcmp : caseSensitive ? strcmp : strcmpi));
-        } else {
-            qsort_v1((void **) lineptr, 0, nlines - 1,
-                     (int (*)(void *, void *)) (numeric ? numcmp_reverse : caseSensitive ? strcmp_reverse
-                                                                                         : strcmpi_reverse));
-        }
+        qsort_v1((void **) lineptr, 0, nlines - 1,
+                 (int (*)(void *, void *)) cmp);
         printf("\n\n");
         writelines(lineptr, nlines);
         return 0;
@@ -120,13 +134,6 @@ void writelines(char *lineptr[], int nlines) {
     int i;
     for (int i = 0; i < nlines; i++) {
         printf("%s\n", lineptr[i]);
-    }
-}
-
-
-void writelines_v2(char *lineptr[], int nlines) {
-    while (nlines-- > 0) {
-        printf("%s\n", *lineptr++);
     }
 }
 
@@ -177,27 +184,67 @@ int getline_v1(char s[], int lim) {
 
 #include <stdlib.h>
 
-int numcmp(char *s1, char *s2) {
-    double v1, v2;
-    v1 = atof(s1);
-    v2 = atof(s2);
-    if (v1 < v2) {
-        return -1;
-    } else if (v1 > v2) {
-        return 1;
+
+int cmp(char *s1, char *s2) {
+    int res;
+    if (numeric) {
+        double v1, v2;
+        v1 = atof(s1);
+        v2 = atof(s2);
+        if (v1 < v2) {
+            res = -1;
+        } else if (v1 > v2) {
+            res = 1;
+        } else {
+            res = 0;
+        }
     } else {
-        return 0;
+        res = strcmp_inner(s1, s2);
+    }
+
+    if (reverse) {
+        return -res;
     }
 }
 
-int numcmp_reverse(char *s1, char *s2) {
-    return -numcmp(s1, s2);
-}
-
-int strcmp_reverse(char *s1, char *s2) {
-    return -strcmp(s1, s2);
-}
-
-int strcmpi_reverse(char *s1, char *s2) {
-    return -strcmpi(s1, s2);
+int strcmp_inner(char *s1, char *s2) {
+    if (alphabetNumberSpaceOnly) {
+        while (1) {
+            //尝试找到第一个满足要求的s1字符
+            while (*s1 != '\0' && !(isdigit(*s1) || isalpha(*s1) || isspace(*s1))) {
+                s1++;
+            }
+            //尝试找到第一个满足要求的s2字符
+            while (*s2 != '\0' && !(isdigit(*s2) || isalpha(*s2) || isspace(*s2))) {
+                s2++;
+            }
+            if (*s1 == '\0' && *s2 == '\0') {
+                return 0;
+            } else if (*s1 == '\0' && *s2 != '\0') {
+                return -1;
+            } else if (*s1 != '\0' && *s2 == '\0') {
+                return 1;
+            } else {
+                //比较两个字符
+                if (!caseSensitive) {
+                    int temp1 = tolower(*s1);
+                    int temp2 = tolower(*s2);
+                    if (temp1 != temp2) {
+                        return temp1 - temp2;
+                    }
+                } else if (*s1 != *s2) {
+                    return *s1 - *s2;
+                }
+                //比较下一个字符
+                s1++;
+                s2++;
+            }
+        }
+    } else {
+        if (caseSensitive) {
+            return strcmp(s1, s2);
+        } else {
+            return strcmpi(s1, s2);
+        }
+    }
 }
